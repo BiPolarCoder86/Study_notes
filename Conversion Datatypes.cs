631069d3835743cb6c6cf4f456865156a6cd37a8
Derek Underwood <biploarcoder@outlook.com>
2021-12-19 13:57:02 -0600

    Update Conversion Datatypes.cs
    Removed unneeded commentary

--- Conversion Datatypes.cs
+++ Conversion Datatypes.cs
@@ -1,28 +1,5 @@
 ﻿using System;
-/*
-namespace TypeConversion
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            try
-            {
-                var number = "1234";
-                byte b = Convert.ToByte(number);
-                Console.WriteLine(number);
-            }
-            catch (Exception)
-            {
-                Console.WriteLine("The number could not be converted to byte");
-            }
-          
 
-        }
-    }
-}
-
-*/
 
 namespace DataConversion
 {

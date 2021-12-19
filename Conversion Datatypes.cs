a5f4cd92c98b5b0c111ebacef618a734d71cefec
Derek Underwood <biploarcoder@outlook.com>
2021-12-19 13:52:53 -0600

    Create Conversion Datatypes.cs

---
+++ Conversion Datatypes.cs
@@ -0,0 +1,51 @@
+﻿using System;
+/*
+namespace TypeConversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var number = "1234";
+                byte b = Convert.ToByte(number);
+                Console.WriteLine(number);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("The number could not be converted to byte");
+            }
+          
+
+        }
+    }
+}
+
+*/
+
+namespace DataConversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string str = "True";
+                bool b = Convert.ToBoolean(str);
+               Console.WriteLine(b);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("The string could not be converted");
+            }
+
+
+        }
+    }
+}
+
+
+
+
